3eb9130755555ff293a463924ffb2645ca679824
migregal <devmigregal@gmail.com>
2021-09-26 21:57:43 +0300

    Args parser added

---
+++ utils/args.go
@@ -0,0 +1,37 @@
+package utils
+
+import "errors"
+
+type Args struct {
+	Host  string
+	Port  string
+	Token string
+	Scope string
+}
+
+func ParseArgs(args []string) (*Args, error) {
+	if len(args) < 5 {
+		return nil, errors.New("missing args detected")
+	}
+
+	if args[1] == "" {
+		return nil, errors.New("missing host value")
+	}
+
+	if args[2] == "" {
+		return nil, errors.New("missing port value")
+	}
+
+	if args[3] == "" {
+		return nil, errors.New("missing token value")
+	}
+
+	res := Args{
+		Host:  args[1],
+		Port:  args[2],
+		Token: args[3],
+		Scope: args[4],
+	}
+
+	return &res, nil
+}
