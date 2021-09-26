dfba014b00972d73512ff744955d1af2f27e63ac
migregal <devmigregal@gmail.com>
2021-09-26 19:05:50 +0300

    Response decoder added

---
+++ client/decoder.go
@@ -0,0 +1,152 @@
+package client
+
+import (
+	"encoding/binary"
+	"errors"
+)
+
+type Decoder struct{}
+
+func (d Decoder) DecodeResponse(response []byte) (*CubeResponseBody, error) {
+	b := make([]byte, len(response))
+	if copy(b, response) != len(response) {
+		return nil, errors.New("failed to decode response")
+	}
+
+	h, err := decodeHeader(&b)
+	if err != nil {
+		return nil, err
+	}
+
+	b = b[:h.bodyLength]
+	return decodeBody(&b)
+}
+
+func decodeHeader(b *[]byte) (*CubeHeader, error) {
+	if len(*b) < 12 { // 3 numbers of header
+		return nil, errors.New("byte slice is too short for header")
+	}
+	header := CubeHeader{}
+
+	var err error
+	header.svcId, err = extractInt(b)
+	if err != nil {
+		return nil, err
+	}
+
+	header.bodyLength, err = extractInt(b)
+	if err != nil {
+		return nil, err
+	}
+
+	header.requestId, err = extractInt(b)
+	if err != nil {
+		return nil, err
+	}
+
+	return &header, nil
+}
+
+func decodeBody(b *[]byte) (*CubeResponseBody, error) {
+	rc, err := extractInt(b)
+	if err != nil {
+		return nil, err
+	}
+
+	body := CubeResponseBody{ReturnCode: rc}
+	if rc != ok {
+		body.ErrString, err = extractString(b)
+		if err != nil {
+			return nil, err
+		}
+		return &body, nil
+	}
+
+	body.ClientId, err = extractString(b)
+	if err != nil {
+		return nil, err
+	}
+
+	body.ClientType, err = extractInt(b)
+	if err != nil {
+		return nil, err
+	}
+
+	body.Username, err = extractString(b)
+	if err != nil {
+		return nil, err
+	}
+
+	body.ExpiresIn, err = extractInt(b)
+	if err != nil {
+		return nil, err
+	}
+
+	body.UserId, err = extractInt64(b)
+	if err != nil {
+		return nil, err
+	}
+
+	return &body, nil
+}
+
+func extractInt(b *[]byte) (int32, error) {
+	if b == nil {
+		return 0, errors.New("empty ptr passed to extract int")
+	}
+
+	if len(*b) < 4 {
+		return 0, errors.New("too short data passed for int extraction")
+	}
+
+	defer func() {
+		temp := *b
+		*b = temp[4:]
+	}()
+
+	return int32(binary.LittleEndian.Uint32(*b)), nil
+}
+
+func extractInt64(b *[]byte) (int64, error) {
+	if b == nil {
+		return 0, errors.New("empty ptr passed to extract int")
+	}
+
+	if len(*b) < 8 {
+		return 0, errors.New("too short data passed for int64 extraction")
+	}
+
+	defer func() {
+		temp := *b
+		*b = temp[8:]
+	}()
+
+	return int64(binary.LittleEndian.Uint64(*b)), nil
+}
+
+func extractString(b *[]byte) (string, error) {
+	if b == nil {
+		return "", errors.New("empty ptr passed to extract string")
+	}
+
+	if len(*b) < 4 {
+		return "", errors.New("too short data passed for string len extraction")
+	}
+
+	l, err := extractInt(b)
+	if err != nil {
+		return "", err
+	}
+
+	// as int >= int32, it's correct to convert l to int from int32
+	if int(l) > len(*b) {
+		return "", errors.New("too short data passed for string extraction")
+	}
+
+	defer func() {
+		temp := *b
+		*b = temp[l:]
+	}()
+
+	return string((*b)[:l]), nil
+}
---
+++ client/decoder_test.go
@@ -0,0 +1,197 @@
+package client
+
+import (
+	"testing"
+)
+
+func TestDecodeExtractInt(t *testing.T) {
+	b := []byte{123, 0, 0, 0}
+	v, err := extractInt(&b)
+	if err != nil {
+		t.Fatal("Extracting of int failed")
+	}
+
+	if v != 123 {
+		t.Fatal("Extracting of int returned wrong value")
+	}
+
+	if len(b) != 0 {
+		t.Fatal("Extracting of int returned non-empty slice")
+	}
+}
+
+func TestDecodeExtractInt64(t *testing.T) {
+	b := []byte{123, 0, 0, 0, 0, 0, 0, 0}
+	v, err := extractInt64(&b)
+	if err != nil {
+		t.Fatal("Extracting of int failed")
+	}
+
+	if v != 123 {
+		t.Fatal("Extracting of int returned wrong value")
+	}
+
+	if len(b) != 0 {
+		t.Fatal("Extracting of int returned non-empty slice")
+	}
+}
+
+func TestDecoderExtractString(t *testing.T) {
+	b := []byte{
+		11, 0, 0, 0,
+		116, 101, 115, 116, 32, 115, 116, 114, 105, 110, 103,
+	}
+	v, err := extractString(&b)
+	if err != nil {
+		t.Fatal("Extracting of string failed")
+	}
+
+	if v != "test string" {
+		t.Fatal("Extracting of string returned wrong value")
+	}
+}
+
+func TestDecoderDecodeHeader(t *testing.T) {
+	b := []byte{
+		11, 0, 0, 0, // svc_id
+		22, 0, 0, 0, // body_length
+		33, 130, 101, 77, // requestId
+	}
+
+	header, err := decodeHeader(&b)
+	if err != nil {
+		t.Fatal("Extracting of string failed")
+	}
+
+	if header.svcId != 11 {
+		t.Fatal("Extracting of header returned wrong svc_id value")
+	}
+
+	if header.bodyLength != 22 {
+		t.Fatal("Extracting of header returned wrong body_length value")
+	}
+
+	if header.requestId != 1298498081 {
+		t.Fatal("Extracting of header returned wrong request_id value")
+	}
+}
+
+func TestDecoderDecodeNilResponse(t *testing.T) {
+	_, err := decodeBody(nil)
+	if err == nil {
+		t.Fatal("Extracting of nil response successed")
+	}
+}
+
+func TestDecoderDecodeBrokenResponseBody(t *testing.T) {
+	b := []byte{
+		0, 0, 0, // broken return code
+	}
+
+	_, err := decodeBody(&b)
+	if err == nil {
+		t.Fatal("Extracting of broken response body successed")
+	}
+}
+
+func TestDecoderDecodeBrokenResponseBody2(t *testing.T) {
+	b := []byte{
+		0, 0, 0, 0, // return code == 0
+		17, 0, 0, // broken string length
+	}
+
+	_, err := decodeBody(&b)
+	if err == nil {
+		t.Fatal("Extracting of broken response body successed")
+	}
+}
+
+func TestDecoderDecodeBrokenResponseBody3(t *testing.T) {
+	b := []byte{
+		0, 0, 0, 0, // return code == 0
+		17, 0, 0, 0,
+		99, 108, 105, 101, 110, // broken string
+	}
+
+	_, err := decodeBody(&b)
+	if err == nil {
+		t.Fatal("Extracting of broken response body successed")
+	}
+}
+
+func TestDecoderDecodeBrokenResponseBody4(t *testing.T) {
+	b := []byte{
+		0, 0, 0, 0, // return code == 0
+		17, 0, 0, 0,
+		99, 108, 105, 101, 110,
+		116, 32, 105, 100, 101, 110,
+		116, 105, 102, 105, 101, 114, // client_id == "client identifier"
+		4, 208, 34, 0, // client_type == 2281476
+		7, 0, 0, 0,
+		97, 119, 101, 115, 111, 109, 101, // username == "awesome"
+		192, 146, 80, 97, // expires_in == 1632670400
+		32, 83, 105, 0, 13, // broken user_id
+	}
+
+	_, err := decodeBody(&b)
+	if err == nil {
+		t.Fatal("Extracting of broken response body successed")
+	}
+}
+
+func TestDecoderDecodeResponseBody(t *testing.T) {
+	b := []byte{
+		0, 0, 0, 0, // return code == 0
+		17, 0, 0, 0,
+		99, 108, 105, 101, 110,
+		116, 32, 105, 100, 101, 110,
+		116, 105, 102, 105, 101, 114, // client_id == "client identifier"
+		4, 208, 34, 0, // client_type == 2281476
+		7, 0, 0, 0,
+		97, 119, 101, 115, 111, 109, 101, // username == "awesome"
+		192, 146, 80, 97, // expires_in == 1632670400
+		32, 83, 105, 0, 13, 1, 58, 0, // user_id == 16326704002323232
+	}
+
+	body, err := decodeBody(&b)
+	if err != nil {
+		t.Fatal("Extracting of string failed")
+	}
+
+	expected := CubeResponseBody{
+		ReturnCode: 0,
+		ClientId:   "client identifier",
+		ClientType: 2281476,
+		Username:   "awesome",
+		ExpiresIn:  1632670400,
+		UserId:     16326704002323232,
+	}
+
+	if *body != expected {
+		t.Fatal("Extracting of body returned wrong field(-s) value(-es)")
+	}
+}
+
+func TestDecoderDecodeErrorResponseBody(t *testing.T) {
+	b := []byte{
+		1, 0, 0, 0, // return code == 1
+		17, 0, 0, 0,
+		99, 108, 105, 101, 110,
+		116, 32, 105, 100, 101, 110,
+		116, 105, 102, 105, 101, 114, // error_msg == "client identifier"
+	}
+
+	body, err := decodeBody(&b)
+	if err != nil {
+		t.Fatal("Extracting of string failed")
+	}
+
+	expected := CubeResponseBody{
+		ReturnCode: 1,
+		ErrString:  "client identifier",
+	}
+
+	if *body != expected {
+		t.Fatal("Extracting of body returned wrong field(-s) value(-es)")
+	}
+}
