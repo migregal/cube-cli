1e99ec29ce26217cc1423af76745ae6e3c8a82e2
migregal <devmigregal@gmail.com>
2021-09-26 22:16:26 +0300

    Moved to iota usage

--- client/response.go
+++ client/response.go
@@ -6,13 +6,13 @@ import (
 )
 
 const (
-	ok            int32 = 0x00000000
-	tokenNotFound int32 = 0x00000001
-	dbError       int32 = 0x00000002
-	unknownMsg    int32 = 0x00000003
-	badPacket     int32 = 0x00000004
-	badClient     int32 = 0x00000005
-	badScope      int32 = 0x00000006
+	ok int32 = iota
+	tokenNotFound
+	dbError
+	unknownMsg
+	badPacket
+	badClient
+	badScope
 )
 
 const (
