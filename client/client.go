bbe6cf61f902b97c636055540fe2cf2a73cb46c1
migregal <devmigregal@gmail.com>
2021-09-26 21:26:00 +0300

    Method for connection to server added

--- client/client.go
+++ client/client.go
@@ -1,5 +1,12 @@
 package client
 
+import (
+	"bytes"
+	"errors"
+	"io"
+	"net"
+)
+
 const TokenVerifySvcMsg = 0x00000001
 
 const (
@@ -11,3 +18,53 @@ const (
 	badClient     int32 = 0x00000005
 	badScope      int32 = 0x00000006
 )
+
+type cubeClient struct {
+	svcId int32
+	addr  *net.TCPAddr
+}
+
+func NewClient(svcId int32, host string, port string) (*cubeClient, error) {
+	addr, err := net.ResolveTCPAddr("tcp", host+":"+port)
+	if err != nil {
+		return nil, err
+	}
+
+	client := cubeClient{svcId: svcId, addr: addr}
+	return &client, nil
+}
+
+func (c *cubeClient) VerifyToken(token, scope string) (*CubeResponseBody, error) {
+	conn, err := net.DialTCP("tcp", nil, c.addr)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	req := CubeRequestBody{SvcId: c.svcId, Token: token, Scope: scope}
+	_, bin, err := Encoder{}.FormatRequest(&req)
+	if err != nil {
+		return nil, err
+	}
+
+	n, err := conn.Write(bin)
+	if err != nil {
+		return nil, err
+	}
+
+	if n != len(bin) {
+		return nil, errors.New("failed to send request")
+	}
+
+	var buf bytes.Buffer
+	if _, err = io.Copy(&buf, conn); err != nil {
+		return nil, err
+	}
+
+	var resp *CubeResponseBody
+	resp, err = Decoder{}.DecodeResponse(buf.Bytes())
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
